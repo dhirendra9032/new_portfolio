d7d49c75d8d78da7b4bb1893a43d030c1e2c182b
Dhirendra Kumar <74555715+dhirendra9032@users.noreply.github.com>
2021-10-28 18:57:50 +0530

    Update ProjectDetail.js
    trivago clone deploy link changed

--- src/Component/Project/ProjectDetail.js
+++ src/Component/Project/ProjectDetail.js
@@ -15,7 +15,7 @@ export const project = [
         img: trivago,
         name: "Trivago Website Clone",
         detail: "trivago is a hotel search with an extensive price comparison. User can login/register , search hotel and book the hotel",
-        demo: "https://drive.google.com/file/d/1hv4bo_EBkh45Ivr0Ru3MV7KWWOT3rgde/view?usp=sharing",
+        demo: "https://trivagoclone.netlify.app/",
         github: "https://github.com/dhirendra9032/TrivagoClone/tree/master/trivago_clone",
         teck_stack: ["React Js", "CSS", "Node Js"]
 
